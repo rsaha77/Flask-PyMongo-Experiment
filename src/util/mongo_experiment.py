3d598a84de3b6f9a468945ff2ee0ade3c41500c3
ranasaha <rana.saha@sap.com>
2018-01-19 15:55:12 +0800

    Upsert

--- src/util/mongo_experiment.py
+++ src/util/mongo_experiment.py
@@ -6,7 +6,8 @@ import pprint
 app = Flask(__name__)
 
 app.config['MONGO_DBNAME'] = 'my_mongo_experiments'
-app.config['MONGO_URI'] = 'mongodb://' + os.environ.get('DB_USER') + ':' + os.environ.get('DB_PASS') + '@ds127536.mlab.com:27536/my_mongo_experiments'
+app.config['MONGO_URI'] = 'mongodb://' + os.environ.get('DB_USER') + ':' + os.environ.get(
+    'DB_PASS') + '@ds127536.mlab.com:27536/my_mongo_experiments'
 
 mongo = PyMongo(app)
 
@@ -18,7 +19,6 @@ def first_page():
 
 @app.route('/find')
 def find():
-    
     about_me = mongo.db.about_me
     rana_details = about_me.find_one({'Name': 'Rana'})
     print("Here: ", rana_details)
@@ -27,7 +27,17 @@ def find():
     for doc in about_me_list:
         print(doc)
 
-    return 'Rana\'s Designation: ' + rana_details['Designation']
+    ret = 'Rana\'s Designation: ' + rana_details['Designation'] + ', Rana\'s Age: ' + str(rana_details['Age'])
+    return ret
+
+
+@app.route('/upsert')
+def upsert():
+    about_me = mongo.db.about_me
+    about_me.update({'Name': 'Rana'}, {"$set": {'Age': 25}})
+    rana_details = about_me.find_one({'Name': 'Rana'})
+    print("Here: ", rana_details)
+    return str(rana_details['Age'])
 
 
 if __name__ == '__main__':
